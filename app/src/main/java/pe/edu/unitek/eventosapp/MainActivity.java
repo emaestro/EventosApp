0cc1a9a0cbf53ea1f7c83433a039a6b8b8579a92
emaestro <f@emaestro.net>
2017-09-26 17:07:50 -0500

    Tomamos el control del TextView

--- app/src/main/java/pe/edu/unitek/eventosapp/MainActivity.java
+++ app/src/main/java/pe/edu/unitek/eventosapp/MainActivity.java
@@ -4,19 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private Button mi_boton;
+    private TextView mi_texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mi_texto = (TextView) findViewById(R.id.textView);
+
         mi_boton = (Button) findViewById(R.id.button);
         mi_boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
