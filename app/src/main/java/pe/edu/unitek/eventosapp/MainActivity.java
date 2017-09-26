f5e4f7c8e3fc325f49687e49fa97e7373b92bd8d
emaestro <f@emaestro.net>
2017-09-26 16:28:17 -0500

    Agregamos un atributo

--- app/src/main/java/pe/edu/unitek/eventosapp/MainActivity.java
+++ app/src/main/java/pe/edu/unitek/eventosapp/MainActivity.java
@@ -2,12 +2,15 @@ package pe.edu.unitek.eventosapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mi_boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mi_boton = (Button) findViewById(R.id.button);
     }
 }
